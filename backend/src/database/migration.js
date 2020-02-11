9e769d156c15a64e1b45163db6c8ed631e940f64
b7Rexx <bij.maharjan@gmail.com>
2020-02-11 10:52:50 +0545

    docker hub push update

--- backend/src/database/migration.js
+++ backend/src/database/migration.js
@@ -1,28 +1,3 @@
-// const env = require('dotenv').config().parsed;
-//
-// // get the client
-// const mysql = require('mysql2');
-//
-// // create the connection to database
-// const connection = mysql.createConnection({
-//   // host: env.NODE_DB_HOST,
-//   host: env.NODE_DB_HOST,
-//   user: env.NODE_DB_USERNAME,
-//   password: env.NODE_DB_PASSWORD,
-//   database: env.NODE_DB_DATABASE
-// });
-//
-//
-// connection.connect(function(err) {
-//   if (err) {
-//     console.log("Not Connected! > >>  > > >> > > > > > >> > > > >> > > > > > >> > > > > > >> > ");
-//
-//     throw err;
-//   }
-//   console.log("Connected! > >>  > > >> > > > > > >> > > > >> > > > > > >> > > > > > >> > ");
-// });
-
-
 const users = require('../models/user');
 const teams = require('../models/team');
 const tournaments = require('../models/tournament');
